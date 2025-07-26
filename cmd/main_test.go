c7147ef8d26ed44bbe72364b8b4e9ae9947c16f3
Heathcliff <heathcliff@heathcliff.eu>
2025-07-26 15:53:25 +0200

    main.go: Add unit-tests for scenarios resulting in early exit
    Test cases where main.go calls os.Exit with or without error.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,4 +27,60 @@ func TestServerRootHandler(t *testing.T) {
 	assert.Contains(body, "<html>")
 	assert.Contains(body, "</html>")
 	assert.Contains(body, "<a href='/metrics'>")
+}
+
+func TestShowVersion(t *testing.T) {
+	if os.Getenv("RUN_CRASH_TEST") == "1" {
+		_ = flag.CommandLine.Parse([]string{"-version"})
+		main()
+	}
+	execExitTest(t, "TestShowVersion", false)
+}
+
+func TestErrorLoadConfiguration(t *testing.T) {
+	if os.Getenv("RUN_CRASH_TEST") == "1" {
+		_ = flag.CommandLine.Parse([]string{"-config", "/not/a/valid/path"})
+		main()
+	}
+	execExitTest(t, "TestErrorLoadConfiguration", true)
+}
+
+func TestErrorCreateSaveCollector(t *testing.T) {
+	if os.Getenv("RUN_CRASH_TEST") == "1" {
+		_ = flag.CommandLine.Parse([]string{"-config", "testdata/invalid-world-dir.yaml"})
+		main()
+	}
+	execExitTest(t, "TestErrorCreateSaveCollector", true)
+}
+
+func TestErrorCreateRCONCollector(t *testing.T) {
+	if os.Getenv("RUN_CRASH_TEST") == "1" {
+		_ = flag.CommandLine.Parse([]string{"-config", "testdata/invalid-rcon.yaml"})
+		main()
+	}
+	execExitTest(t, "TestErrorCreateRCONCollector", true)
+}
+
+func TestErrorStartServer(t *testing.T) {
+	if os.Getenv("RUN_CRASH_TEST") == "1" {
+		_ = flag.CommandLine.Parse([]string{"-config", "testdata/invalid-port.yaml"})
+		main()
+	}
+	execExitTest(t, "TestErrorStartServer", true)
+}
+
+func execExitTest(t *testing.T, test string, exitsError bool) {
+	cmd := exec.Command(os.Args[0], "-test.run="+test)
+	cmd.Env = append(os.Environ(), "RUN_CRASH_TEST=1")
+	out, err := cmd.CombinedOutput()
+	t.Log("Output:\n", string(out))
+	if exitsError && err == nil {
+		t.Fatal("Process exited without error")
+	} else if !exitsError && err == nil {
+		return
+	}
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
